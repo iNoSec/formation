f4d63e1af250a63fac18a58d7fd3bafb8de0435f
rl1k <inosec@gmail.com>
2019-06-03 14:14:29 +0200

    commit depuis visual studio

--- javascript/TP/divers/child.js
+++ javascript/TP/divers/child.js
@@ -1,6 +1,11 @@
-var div = document.getElementById('myDiv');
-        var first = div.firstChild;
-        var last =  div.lastChild;
+var paragraph = document.getElementById('myP');
+        var first = paragraph.firstChild;//on prend le 1er enfant de l'element ayant pour id myP
+        var last =  paragraph.lastElementChild;//on prend le dernier enfant de l'element ayant pour id myP
 
         alert(first.nodeName.toLowerCase());
+        alert("La on utilise nodeValue : "first.nodeValue);
+        alert("La on utilise data : " + first.data);
+
         alert(last.nodeName.toLowerCase());
+        alert("La on utilise nodeValue : " + last.firstChild.nodeValue);//on prend le firstchild ici pour en avoir le texte
+        alert("La on utilise data : " + last.firstChild.data);//data et nodeValue c'est pareil!!
