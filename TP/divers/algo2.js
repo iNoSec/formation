abe86a061c5d414e3ff710548408d0f15bae697b
pj <piereaupjpeje@gmail.com>
2019-06-14 09:06:17 +0200

    fin exo 4

--- TP/divers/algo2.js
+++ TP/divers/algo2.js
@@ -1,8 +1,27 @@
-var tab = [4,6,2,1,6,4,6,2,1,6,4,6,2,1,6]
+/* Exercice 1
+var bool = false
+var tab = [4,6,10,14]
 var i = 6
 var e = i
-var j = 0
-/*
+*/
+
+
+/*exo2
+var tab = [4,6,10,14]
+var i = 0
+var e = 10*/
+
+/*exo3
+var tab = [4,6,10,14]
+var e = 6
+var j = 0*/
+
+/*exo4*/
+var tab = [4,6,8]
+var i = 0
+var e = 10
+
+/*Exo 1
 for (i=0; i<= tab.length;i++){
     if (tab[i]==e){
     bool = true;
@@ -10,21 +29,48 @@ for (i=0; i<= tab.length;i++){
     console.log("Bravo tu as un 6 dans ton tableau a la " + index_convert + "e place du tableau" );
     }
 }*/
-/*
+
+
+/*Exo 2 
 while ((tab[i] != e) && (i < tab.length)){
         i++;
 }
 if( i >= tab.length){
-    console.log("tu n'as pas de 6")
+    console.log("tu n'as pas de 10")
 }
 else{
-console.log("tu as un 6 a l'index" + i )
+console.log("tu as un 10 a l'index" + i )
 }*/
 
-
+/*
 for (i=0; i<= tab.length;i++){
     if (tab[i]==e){
         j++
     }
 }
-console.log("Bravo tu as un 6 et il est present " + j + "fois" );
+console.log("Bravo tu as un 6 et il est present " + j + "fois" );*/
+//Exo 3
+/*
+while ((tab[i] != e) && (i < tab.length)){
+        i++;
+}
+if( i >= tab.length){
+    console.log("tu n'as pas de 10")
+}
+else{
+console.log("tu as un 10 a l'index" + i )
+}*/
+
+//Exo 4
+while ((tab[i] != e) && (tab[i]<e)){
+        i++;
+}
+if( tab[i] == e){
+    convert = i +1 
+console.log("tu as un 10 a l'index" + i + " ou si tu preferes a la " + convert + "e place du tableau")
+
+}
+else{
+    console.log("tu n'as pas de 10")
+
+}
